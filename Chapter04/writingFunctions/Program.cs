7ffad8ce8addb894597f20e879a5200192a6cf15
litesh1998 <49069637+litesh1998@users.noreply.github.com>
2021-02-08 20:11:00 +0530

    Writting Functions Without Return

---
+++ Chapter04/writingFunctions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Console;
+
+namespace writingFunctions
+{
+    class Program
+    {
+        static void PrintTable(byte number){
+            WriteLine($"Printing The Table of {number}");
+            for( int i=1; i<=10; i++){
+                WriteLine($"{number}  X  {i,2} = {number*i,2}");
+            }
+        }
+
+        static void RunTable(){
+            bool IsNumber;
+
+            do {
+
+                WriteLine("Enter a Number between 0 and 255");
+                IsNumber = byte.TryParse(ReadLine(), out byte number);
+
+                if(IsNumber){
+                    PrintTable(number);
+                }
+                else{
+                    WriteLine("The Number is not between 0 and 255");
+                }
+                
+            }
+            while(IsNumber);
+        }
+        static void Main(string[] args)
+        {
+            RunTable();
+        }
+    }
+}
