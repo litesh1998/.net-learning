d77003e09af91da001b3b5ecc4ba0de9b19d6311
litesh1998 <49069637+litesh1998@users.noreply.github.com>
2021-02-08 21:42:08 +0530

    leared basic debugging and trace and debug functions

---
+++ Chapter04/Debugging/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trace.Listeners.Add(new TextWriterTraceListener(File.CreateText("log.txt")));
+
+            Trace.AutoFlush = true;
+            Trace.WriteLine("Trace Says, I am Listening");
+            Debug.WriteLine("Debug Says, I am Listening");
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json",
+                optional: true, reloadOnChange: true);
+            IConfigurationRoot configuration = builder.Build();
+            var ts = new TraceSwitch(
+            displayName: "PacktSwitch",
+            description: "This switch is set via a JSON config.");
+            configuration.GetSection("PacktSwitch").Bind(ts);
+            Trace.WriteLineIf(ts.TraceError, "Trace error");
+            Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+            Trace.WriteLineIf(ts.TraceInfo, "Trace information");
+            Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
+        }
+    }
+}
