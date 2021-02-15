520ae34a9c7110b93158477f91d52377cf7efbcd
litesh1998 <49069637+litesh1998@users.noreply.github.com>
2021-02-15 15:19:07 +0530

    Learned Basics of Class and Constructors

--- Chapter05/PacktLibrary/Person.cs
+++ Chapter05/PacktLibrary/Person.cs
@@ -1,10 +1,30 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Packt.Shared
 {
     public class Person
     {
         public string Name;
         public DateTime DateOfBirth;
+        public WondersOfTheAncientWorld FavoriteAncientWonder;
+        public WondersOfTheAncientWorld BucketList;
+        public List<Person> Children = new List<Person>();
+
+        public readonly DateTime Instantiated;
+
+        public readonly string HomePlanet = "Earth";
+
+        public Person()
+        {
+            Name = "Unknown";
+            Instantiated = DateTime.Now;
+        }
+
+        public Person(string initialName, string homePlanet)
+        {
+            Name = initialName;
+            HomePlanet = homePlanet;
+            Instantiated = DateTime.Now;
+        }
     }
 }
--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -3,6 +3,7 @@ using static System.Console;
 using Packt.Shared;
 
 
+
 namespace PeopleApp
 {
     class Program
@@ -12,13 +13,62 @@ namespace PeopleApp
             // Console.WriteLine("Hello World!");
             var bob = new Person();
             // WriteLine(bob.ToString());
-            bob.Name="Bob Smith";
-            bob.DateOfBirth= new DateTime(1965, 12, 21);
+            bob.Name = "Bob Smith";
+            bob.DateOfBirth = new DateTime(1965, 12, 21);
             WriteLine(
                 format: "{0} was born on {1:dddd, d MMMM yyyy}",
                 arg0: bob.Name,
                 arg1: bob.DateOfBirth
             );
+
+            bob.FavoriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
+            WriteLine(format:
+            "{0}'s favorite wonder is {1}. Its integer is {2}.",
+            arg0: bob.Name,
+            arg1: bob.FavoriteAncientWonder,
+            arg2: (int)bob.FavoriteAncientWonder);
+
+            bob.BucketList =
+                WondersOfTheAncientWorld.HangingGardensOfBabylon
+| WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
+            WriteLine($"{bob.Name}'s bucket list is {bob.BucketList}");
+
+            bob.Children.Add(new Person { Name = "Alfred" });
+            bob.Children.Add(new Person { Name = "Zoe" });
+            WriteLine(
+            $"{bob.Name} has {bob.Children.Count} children:");
+            for (int child = 0; child < bob.Children.Count; child++)
+            {
+                WriteLine($" {bob.Children[child].Name}");
+            }
+
+            BankAccount.InterestRate = 0.012M; // store a shared value
+            var jonesAccount = new BankAccount();
+            jonesAccount.AccountName = "Mrs. Jones";
+            jonesAccount.Balance = 2400;
+            WriteLine(format: "{0} earned {1:C} interest.",
+            arg0: jonesAccount.AccountName,
+            arg1: jonesAccount.Balance * BankAccount.InterestRate);
+            var gerrierAccount = new BankAccount();
+            gerrierAccount.AccountName = "Ms. Gerrier";
+            gerrierAccount.Balance = 98;
+            WriteLine(format: "{0} earned {1:C} interest.",
+            arg0: gerrierAccount.AccountName,
+            arg1: gerrierAccount.Balance * BankAccount.InterestRate);
+
+
+            var blankPerson = new Person();
+            WriteLine(format:
+            "{0} was created at {1:hh:mm:ss} on a {1:dddd}.",
+            arg0: blankPerson.Name,
+            arg1: blankPerson.Instantiated);
+
+            var gunny = new Person("Gunny", "Mars");
+            WriteLine(format:
+            "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+            arg0: gunny.Name,
+            arg1: gunny.HomePlanet,
+            arg2: gunny.Instantiated);
         }
     }
 }
